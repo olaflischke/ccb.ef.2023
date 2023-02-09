6df861b9525ff49a6c68d05cb133852721d1d2d0
Olaf Lischke <olaf.lischke@lischke-edv.de>
2023-02-09 09:30:37 +0100

    OneNote als PDF

--- Live Coding/Northwind/NorthwindDalUnitTests/ContextTests.cs
+++ Live Coding/Northwind/NorthwindDalUnitTests/ContextTests.cs
@@ -14,14 +14,33 @@ namespace NorthwindDalUnitTests
         }
 
         [Test]
-        public  void CustomerCountTest()
+        public void CustomerCountTest()
         {
             DbSet<Customer> customers = context.Customers; //.ToList(); // alle Kunden aus der Customers-Tabelle
 
-           // Console.WriteLine($"Name des ersten Customers: {customers.First().CompanyName}");
+
+
+            // Console.WriteLine($"Name des ersten Customers: {customers.First().CompanyName}");
 
             Assert.AreEqual(93, customers.Count());
         }
 
+        [Test]
+        public void GermanCustomers()
+        {
+            var germans = context.Customers.Where(cu => cu.Country == "Germany");
+                                        //.Select(cu => new DisplayCustomer()
+                                        //{
+                                        //    CompanyName = cu.CompanyName,
+                                        //    ContactName = cu.ContactName
+                                        //});
+        }
+
+    }
+
+    public class DisplayCustomer
+    {
+        public string CompanyName { get; set; }
+        public string ContactName { get; set; }
     }
 }
